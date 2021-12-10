2744490d0fdd195318837ec44fb07db4b8c93bbf
Jean-Guy Rousset <rousset.jean-guy@laposte.net>
2021-12-10 18:09:29 +0100

    update article links script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
 {
   'use strict';
-/*
-document.getElementById('test-button').addEventListener('click', function(){
-  const links = document.querySelectorAll('.titles a');
-  console.log('links: ', links);
-  console.log(typeof links);
-});
-*/
 
   const titleClickHandler = function(event){
+    event.preventDefault(); //prevents from scrolling to the position reffered on the page
     const clickedElement = this;
     console.log('link was clicked!');
-    //console.log(event);
 
     /* remove class 'active' from all article links*/
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -21,9 +14,7 @@ document.getElementById('test-button').addEventListener('click', function(){
 
     /*add class 'active' to the clicked link*/
     console.log('clicked: ', clickedElement);
-    //console.log('clicked + ' + clickedElement);
     clickedElement.classList.add('active');
-
 
     /*remove class 'active' from all articles*/
     const activeArticles = document.querySelectorAll('article.active');
@@ -31,10 +22,19 @@ document.getElementById('test-button').addEventListener('click', function(){
       activeArticle.classList.remove('active');
     }
 
-
     /*get 'href' attribute from the clicked link*/
+    const selectedLink = clickedElement.getAttribute('href');
+    console.log(selectedLink.slice(1));
+
     /*find the correct article using the selector (value of 'href' attribute)*/
+    const selectedArticle = document.getElementById(selectedLink.slice(1));
+
     /*add class 'active' to the correct article */
+    selectedArticle.classList.add('active');
+
+    //one line code:
+    //document.getElementById(clickedElement.getAttribute('href').slice(1)).classList.add('active');
+
   }
 
   const links = document.querySelectorAll('.titles a');
