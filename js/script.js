0fc4569b61e0584e4a7d24ee4466e3c4c41b8b85
Jean-Guy Rousset <rousset.jean-guy@laposte.net>
2021-12-10 09:41:36 +0100

    remove class active from links and article in script.js

--- js/script.js
+++ js/script.js
@@ -10,14 +10,27 @@ document.getElementById('test-button').addEventListener('click', function(){
 
   const titleClickHandler = function(event){
     console.log('link was clicked!');
-    console.log(event);
+    //console.log(event);
 
-    /* remove class 'active' from all article links
-     add class 'active' to the clicked link
-    remove class 'active' from all articles
-    get 'href' attribute from the clicked link
-    find the correct article using the selector (value of 'href' attribute)
-    add class 'active' to the correct article */
+    /* remove class 'active' from all article links*/
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
+
+    /*add class 'active' to the clicked link*/
+
+
+    /*remove class 'active' from all articles*/
+    const activeArticles = document.querySelectorAll('article.active');
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
+
+
+    /*get 'href' attribute from the clicked link*/
+    /*find the correct article using the selector (value of 'href' attribute)*/
+    /*add class 'active' to the correct article */
   }
 
   const links = document.querySelectorAll('.titles a');
